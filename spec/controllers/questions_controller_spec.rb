f3d54dac4064624734d5fd503a66f549d8c27e6f
Dima <sumskoydima@gmail.com>
2017-07-07 22:36:29 +0300

    Test for questions index

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  describe 'GET #index' do
+    let(:questions) { create_list(:question, 2) }
 
+    it 'fills an array of questions' do
+      get :index
+      expect(assigns(:questions)).to match_array(questions)
+    end
+  end
 end
