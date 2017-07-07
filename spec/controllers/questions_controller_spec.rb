835c96f90cdf9e09c426e8b4b75ecfbbd4c1b3cb
Dima <sumskoydima@gmail.com>
2017-07-08 00:52:22 +0300

    Refactor questions index action test

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -4,9 +4,14 @@ RSpec.describe QuestionsController, type: :controller do
   describe 'GET #index' do
     let(:questions) { create_list(:question, 2) }
 
+    before { get :index }
+
     it 'fills an array of questions' do
-      get :index
       expect(assigns(:questions)).to match_array(questions)
+    end
+
+    it 'renders index view' do
+      expect(response).to render_template :index
     end
   end
 end
