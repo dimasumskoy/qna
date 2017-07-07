f65053f305863129b97920bff2e71429eef9e66f
Dima <sumskoydima@gmail.com>
2017-07-08 01:29:15 +0300

    Add questions show action test with refactor

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,17 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
-  describe 'GET #index' do
-    let(:questions) { create_list(:question, 2) }
+  let(:question) { create(:question) }
 
+  describe 'GET #index' do
     before { get :index }
 
     it 'fills an array of questions' do
-      expect(assigns(:questions)).to match_array(questions)
+      expect(assigns(:questions)).to eq([question])
     end
 
     it 'renders index view' do
       expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+    before { get :show, params: { id: question } }
+
+    it 'assigns the requested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'renders show view' do
+      expect(response).to render_template :show
     end
   end
 end
