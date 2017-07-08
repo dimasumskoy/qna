7a2d6134c571aae9b0933293c7a8549a5847735d
Dima <sumskoydima@gmail.com>
2017-07-08 17:59:30 +0300

    Add answers show action test with refactoring

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,4 +2,8 @@ class AnswersController < ApplicationController
   def index
     @answers = Answer.all
   end
+
+  def show
+    @answer = Answer.find(params[:id])
+  end
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,19 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+  let(:answer) do
+    @question = create(:question)
+    create(:answer, question_id: @question.id )
+  end
+
   describe 'GET #index' do
     before do
-      @question = create(:question)
-      @answer = create(:answer, question_id: @question.id )
+      answer
       get :index, params: { question_id: @question }
     end
 
     it 'fills an array of answers to @answers' do
-      expect(assigns(:answers)).to eq([@answer])
+      expect(assigns(:answers)).to eq([answer])
     end
 
     it 'renders index view' do
       expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+    before { get :show, params: { id: answer } }
+
+    it 'assigns the requested answer to @answer' do
+      expect(assigns(:answer)).to eq answer
+    end
+
+    it 'renders show view' do
+      expect(response).to render_template :show
     end
   end
 end
