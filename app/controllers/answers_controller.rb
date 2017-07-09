64ada200302ae72332aa23424031e92d9ea27605
Dima <sumskoydima@gmail.com>
2017-07-09 04:51:50 +0300

    Add answers create action test with refactoring

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,18 +1,37 @@
 class AnswersController < ApplicationController
+  before_action :set_answer, only: [:show]
+  before_action :set_question, only: [:new, :create]
+
   def index
     @answers = Answer.all
   end
 
   def show
-    @answer = Answer.find(params[:id])
   end
 
   def new
-    @question = Question.find(params[:question_id])
     @answer = @question.answers.build
   end
 
+  def create
+    @answer = @question.answers.new(answer_params)
+
+    if @answer.save
+      redirect_to @answer
+    else
+      render :new
+    end
+  end
+
   private
+
+  def set_question
+    @question = Question.find(params[:question_id])
+  end
+
+  def set_answer
+    @answer = Answer.find(params[:id])
+  end
 
   def answer_params
     params.require(:answer).permit(:body)
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -39,4 +39,32 @@ RSpec.describe AnswersController, type: :controller do
       expect(response).to render_template :new
     end
   end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      let(:valid_answer_attributes) { post :create, params: { answer: attributes_for(:answer), question_id: question } }
+
+      it 'saves the answer to question in db' do
+        expect { valid_answer_attributes }.to change(Answer, :count).by(1)
+      end
+
+      it 'redirects to answer show view' do
+        valid_answer_attributes
+        expect(response).to redirect_to answer_path(assigns(:answer))
+      end
+    end
+
+    context 'with invalid attributes' do
+      let(:invalid_answer_attributes) { post :create, params: { answer: attributes_for(:invalid_answer), question_id: question } }
+
+      it 'does not save the answer to question in db' do
+        expect { invalid_answer_attributes }.to_not change(Answer, :count)
+      end
+
+      it 're-renders new view' do
+        invalid_answer_attributes
+        expect(response).to render_template :new
+      end
+    end
+  end
 end
