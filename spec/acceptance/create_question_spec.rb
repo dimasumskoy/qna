1a5704ea4d5c95a6957fd7168ebb0acb5e60eb83
Dima <sumskoydima@gmail.com>
2017-07-13 05:51:42 +0300

    Add acceptance for create question

--- spec/acceptance/create_question_spec.rb
+++ spec/acceptance/create_question_spec.rb
@@ -6,19 +6,25 @@ feature 'Create question', %q{
   I want to be able to ask a question
 } do
   scenario 'Authorized user creates a question' do
-    User.create!(email: 'user@test.com', password: '12345')
+    User.create!(email: 'user@test.com', password: '123456')
 
     visit new_user_session_path
     fill_in 'Email', with: 'user@test.com'
-    fill_in 'Password', with: '12345'
+    fill_in 'Password', with: '123456'
     click_on 'Log in'
 
     visit questions_path
-    click_on 'Create question'
+    click_on 'Ask question'
     fill_in 'Title', with: 'title'
     fill_in 'Body', with: 'text'
-    click_ion 'Create'
+    click_on 'Create'
 
-    expect(page).to have_content 'Your question successfully created.'
+    expect(page).to have_content 'title'
+  end
+
+  scenario 'Unauthorized user tries to create a question' do
+    visit questions_path
+    click_on 'Ask question'
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
   end
 end
