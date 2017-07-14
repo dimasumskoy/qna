82bd235941ed6f60fc0ef4c37f9835aa9adc2e1f
Dima <sumskoydima@gmail.com>
2017-07-14 04:51:09 +0300

    Add acceptance test for questions index

---
+++ spec/acceptance/questions/all_questions_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+feature 'List all questions', %q{
+  In order to find the question
+  As a user
+  I want to be able to see the list of all questions
+} do
+  scenario 'Any user can see the list of questions' do
+    @all_questions = create_list(:question, 3)
+
+    visit questions_path
+    expect(page).to have_content @all_questions[rand(0..3)].title
+  end
+end
