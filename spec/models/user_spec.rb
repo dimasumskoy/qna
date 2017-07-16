6623c6eba587ca1849e7773f2d8da964d56221ae
Dima <sumskoydima@gmail.com>
2017-07-16 12:23:49 +0300

    Add model test for #author_of?

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,18 @@ RSpec.describe User, type: :model do
 
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
+
+  let(:users) { create_list(:user, 2) }
+  let(:user_question) { create(:question, user: users[0]) }
+  let(:not_user_question) { create(:question, user: users[1]) }
+
+  context '#author_of?' do
+    it 'returns true' do
+      expect(users[0].author_of?(user_question)).to be_truthy
+    end
+
+    it 'returns false' do
+      expect(users[0].author_of?(not_user_question)).to be_falsey
+    end
+  end
 end
