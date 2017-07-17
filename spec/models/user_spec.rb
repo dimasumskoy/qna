47f19e010d9839494b3b29179849a820a55558c3
Dima <sumskoydima@gmail.com>
2017-07-17 08:30:35 +0300

    Refactor
    user model spec & question show view

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,17 +7,17 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
 
-  let(:users) { create_list(:user, 2) }
-  let(:user_question) { create(:question, user: users[0]) }
-  let(:not_user_question) { create(:question, user: users[1]) }
+  let(:user) { create(:user) }
+  let(:user_question) { create(:question, user: user) }
+  let(:not_user_question) { create(:question) }
 
   context '#author_of?' do
     it 'returns true' do
-      expect(users[0].author_of?(user_question)).to be_truthy
+      expect(user).to be_author_of(user_question)
     end
 
     it 'returns false' do
-      expect(users[0].author_of?(not_user_question)).to be_falsey
+      expect(user).to_not be_author_of(not_user_question)
     end
   end
 end
