280402954c3d2a1976e8f885352f1d4b551738fe
Dima <sumskoydima@gmail.com>
2017-07-22 17:34:36 +0300

    Add best answer acceptance spec

---
+++ spec/acceptance/answers/best_answer_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+feature 'Best answer', %q{
+  In order to choose the the most helpful answer
+  As an author of the question
+  I want to be able to mark the answer as the best answer
+} do
+  given(:user) { create(:user) }
+  given(:other_user) { create(:user) }
+  given!(:question) { create(:question, user: user) }
+  given!(:answer) { create(:answer, question: question, user: other_user) }
+
+  scenario 'Author of the question tries to mark the answer as best' do
+    sign_in(user)
+    visit question_path(question)
+
+    expect(page).to have_content answer.body
+    expect(page).to have_link 'Best answer'
+
+    click_on 'Best answer'
+
+    within '.answet_list' do
+      expect(page).to have_selector '.best-answer'
+    end
+  end
+  scenario 'Other authorized user tries to mark the answer as best'
+  scenario 'Unauthorized user tries to mark the answer as best'
+end
