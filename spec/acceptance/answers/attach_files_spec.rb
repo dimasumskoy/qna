82efb2bad08fdb4cad0f2330db0a6c303a5c9a87
Dima <sumskoydima@gmail.com>
2017-07-30 21:05:34 +0300

    Add attachable associations

---
+++ spec/acceptance/answers/attach_files_spec.rb
@@ -0,0 +1,26 @@
+require 'acceptance/acceptance_helper'
+
+feature 'Attach files to question', %q{
+  In order to illustrate the answer
+  As an answer's author
+  I want to be able to attach files
+} do
+  given(:user) { create(:user) }
+  given(:question) { create(:question) }
+
+  background do
+    sign_in(user)
+    visit question_path(question)
+  end
+
+  scenario 'User adds file while answering question' do
+    fill_in 'Answer', with: 'test body'
+    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    click_on 'Reply'
+
+    within '.answer_list' do
+      expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+      expect(page).to have_content 'test body'
+    end
+  end
+end
