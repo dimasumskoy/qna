77bd43c2c3ca772e1183278fcba8cbd3a7b2e67b
Dima <sumskoydima@gmail.com>
2017-08-03 21:42:42 +0300

    Add acceptance spec for voting question

---
+++ spec/acceptance/questions/vote_question_spec.rb
@@ -0,0 +1,44 @@
+require 'acceptance/acceptance_helper'
+
+feature 'Vote for question', %q{
+  In order to agree with the question
+  As an user
+  I want to be able to vote for the question
+} do
+
+  given(:user) { create(:user) }
+  given(:question) { create(:question) }
+  given(:user_question) { create(:question, user: user) }
+
+  scenario 'Authorized user votes for the question', js: true do
+    sign_in(user)
+    visit question_path(question)
+
+    within 'div.question' do
+      click_on '+1'
+      within 'div.question-votes' do
+        expect(question.votes).to eq question.votes + 1
+      end
+    end
+  end
+
+  scenario 'Authorized user cannot vote twice for the same question' do
+    sign_in(user)
+    visit question_path(question)
+
+    within 'div.question' do
+      click_on '+1'
+      expect(page).to have_content 'You cannot vote twice for this question'
+    end
+  end
+
+  scenario 'Authorized user cannot vote for his own question' do
+    sign_in(user)
+    visit question_path(user_question)
+
+    within 'div.question' do
+      expect(page).to_not have_link '+1'
+      expect(page).to_not have_link '-1'
+    end
+  end
+end
