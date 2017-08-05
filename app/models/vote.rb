ba1f9b3700837e4f009f065de3dd25bd601914bb
Dima <sumskoydima@gmail.com>
2017-08-05 20:01:42 +0300

    Add scopes to Vote with specs

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,9 @@
 class Vote < ApplicationRecord
   belongs_to :votable, polymorphic: true
+
+  validates :votable_type, inclusion: %w(Question Answer)
+
+  scope :count_positive, -> { where(value: 1).size }
+  scope :count_negative, -> { where(value: -1).size }
+  scope :count_rating, -> { count_positive - count_negative }
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -2,13 +2,24 @@ require 'rails_helper'
 
 RSpec.describe Vote, type: :model do
   it { should belong_to(:votable) }
+  it { should validate_inclusion_of(:votable_type).in_array(%w(Question Answer)) }
 
   let(:user) { create(:user) }
   let(:question) { create(:question) }
-  let!(:positive_votes) { create_list(:vote, 2, value: 1, user_id: user, votable: question) }
-  let!(:negative_votes) { create_list(:vote, 2, value: -1, user_id: user, votable: question) }
+  let!(:positive_votes) { create_list(:vote, 2, value: 1, user_id: user.id, votable: question) }
+  let!(:negative_votes) { create_list(:vote, 2, value: -1, user_id: user.id, votable: question) }
 
-  context '.positive' do
-    it 'counts positive votes'
+  context 'scopes' do
+    it '.count_positive' do
+      expect(question.votes.count_positive).to eq 2
+    end
+
+    it '.count_negative' do
+      expect(question.votes.count_negative).to eq 2
+    end
+
+    it '.count_rating' do
+      expect(question.votes.count_rating).to eq 0
+    end
   end
 end
