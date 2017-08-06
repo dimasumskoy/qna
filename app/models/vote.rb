b4eae41115426e76636bdacf1601f26772388204
Dima <sumskoydima@gmail.com>
2017-08-06 04:28:59 +0300

    Refactor vote model with specs

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,8 +2,9 @@ class Vote < ApplicationRecord
   belongs_to :votable, polymorphic: true
 
   validates :votable_type, inclusion: %w(Question Answer)
+  validates :user_id, uniqueness: { scope: [:votable_id, :votable_type] }
 
-  scope :count_positive, -> { where(value: 1).size }
-  scope :count_negative, -> { where(value: -1).size }
+  scope :count_positive, -> { where(value: 1).count }
+  scope :count_negative, -> { where(value: -1).count }
   scope :count_rating, -> { count_positive - count_negative }
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -3,19 +3,21 @@ require 'rails_helper'
 RSpec.describe Vote, type: :model do
   it { should belong_to(:votable) }
   it { should validate_inclusion_of(:votable_type).in_array(%w(Question Answer)) }
+  it { should validate_uniqueness_of(:user_id).scoped_to(:votable_id, :votable_type) }
 
   let(:user) { create(:user) }
-  let(:question) { create(:question) }
-  let!(:positive_votes) { create_list(:vote, 2, value: 1, user_id: user.id, votable: question) }
-  let!(:negative_votes) { create_list(:vote, 2, value: -1, user_id: user.id, votable: question) }
+  let(:another_user) { create(:user) }
+  let!(:question) { create(:question) }
+  let!(:positive_vote) { create(:vote, value: 1, user_id: user.id, votable: question) }
+  let!(:negative_vote) { create(:vote, value: -1, user_id: another_user.id, votable: question) }
 
   context 'scopes' do
     it '.count_positive' do
-      expect(question.votes.count_positive).to eq 2
+      expect(question.votes.count_positive).to eq 1
     end
 
     it '.count_negative' do
-      expect(question.votes.count_negative).to eq 2
+      expect(question.votes.count_negative).to eq 1
     end
 
     it '.count_rating' do
