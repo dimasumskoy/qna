1b7c07851682d23bbdc1f37170cfb893ad33fd56
Dima <sumskoydima@gmail.com>
2017-08-24 04:06:37 +0300

    Add acceptance spec for creating comments

--- spec/acceptance/comments/create_comment_spec.rb
+++ spec/acceptance/comments/create_comment_spec.rb
@@ -9,17 +9,36 @@ feature 'Create comment', %q{
   given!(:question) { create(:question) }
 
   context 'creating comment' do
-    scenario 'Authorized user writes a comment' do
+    scenario 'Authorized user writes a comment', js: true do
       sign_in(user)
       visit question_path(question)
 
-      click_on 'Write a comment'
       fill_in 'Comment', with: 'Test comment'
       click_on 'Comment this'
 
-      expect(page).to have_content 'Test comment'
+      within '.comments' do
+        expect(page).to have_content 'Test comment'
+      end
     end
-    scenario 'Unauthorized user tries to write a comment'
-    scenario 'Errors appearance on the comment'
+
+    scenario 'Unauthorized user tries to write a comment', js: true do
+      visit question_path(question)
+
+      fill_in 'Comment', with: 'Test comment'
+      click_on 'Comment this'
+
+      expect(page).to_not have_content 'Test comment'
+      expect(page).to have_content 'You need to sign in or sign up before continuing.'
+    end
+
+    scenario 'Errors appearance on the comment', js: true do
+      sign_in(user)
+      visit question_path(question)
+
+      fill_in 'Comment', with: ''
+      click_on 'Comment this'
+
+      expect(page).to have_content 'Body can\'t be blank'
+    end
   end
 end
