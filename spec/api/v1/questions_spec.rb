ee86af2a5b966fbcd885cc540a847afcb0e4b301
dimasumskoy <sumskoydima@gmail.com>
2017-09-23 11:45:17 +0300

    Add #index API v1 QuestionsController

--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -13,5 +13,27 @@ RSpec.describe Api::V1::QuestionsController, type: :controller do
         expect(response.status).to eq 401
       end
     end
+
+    context 'authorized' do
+      let!(:questions) { create_list(:question, 3) }
+      let(:question) { questions.first }
+      let(:access_token) { create(:access_token) }
+
+      before { get :index, params: { access_token: access_token.token }, format: :json }
+
+      it 'returns status 200' do
+        expect(response).to be_success
+      end
+
+      it 'returns correct amount of questions' do
+        expect(response.body).to have_json_size(questions.size)
+      end
+
+      %w(id created_at updated_at title body).each do |attr|
+        it "returns #{attr} for each question in list" do
+          expect(response.body).to be_json_eql(question.send(attr).to_json).at_path("0/#{attr}")
+        end
+      end
+    end
   end
 end
