88318c71686f9f99a5f5d76fec7eb5bd46c1c8ab
dimasumskoy <sumskoydima@gmail.com>
2017-09-24 15:31:07 +0300

    Refactor API v1 AnswersController

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -1,23 +1,30 @@
 class Api::V1::AnswersController < Api::V1::BaseController
+  before_action :set_question, only: [:index, :create]
+  before_action :set_answer, only: [:show]
   authorize_resource
 
   def index
-    @question = Question.find(params[:question_id])
     respond_with @question.answers
   end
 
   def show
-    @answer = Answer.find(params[:id])
     respond_with @answer
   end
 
   def create
-    @question = Question.find(params[:question_id])
     @answer = @question.answers.create(answer_params.merge(user: current_resource_owner))
     respond_with @answer, each_serializer: AnswerSerializer
   end
 
   private
+
+  def set_question
+    @question = Question.find(params[:question_id])
+  end
+
+  def set_answer
+    @answer = Answer.find(params[:id])
+  end
 
   def answer_params
     params.require(:answer).permit(:body)
