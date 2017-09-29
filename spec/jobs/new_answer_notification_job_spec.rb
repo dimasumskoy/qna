5152df08485b74326ac1cdd7bb20f7eb58bedc09
dimasumskoy <sumskoydima@gmail.com>
2017-09-29 20:47:25 +0300

    Add spec for new answer notification job

--- spec/jobs/new_answer_notification_job_spec.rb
+++ spec/jobs/new_answer_notification_job_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe NewAnswerNotificationJob, type: :job do
+  let(:user) { create(:user) }
+  let(:question) { create(:question, user: user) }
 
+  it 'should send notification to user' do
+    expect(NotificationMailer).to receive(:new_answer).with(user, question).and_call_original
+    NotificationMailer.new_answer(user, question)
+  end
 end
