aea9856647f8d90a6c40eaf0fee90100bf7d0540
dimasumskoy <sumskoydima@gmail.com>
2017-09-29 16:24:04 +0300

    Add spec for .daily_questions scope

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -9,4 +9,17 @@ RSpec.describe Question, type: :model do
   it { should have_many(:answers).dependent(:destroy) }
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
+
+  describe '.daily_questions' do
+    let!(:old_question) { create(:question, created_at: 2.days.ago) }
+    let!(:new_question) { create(:question, created_at: Time.now) }
+
+    it 'should return question for the last 1 day' do
+      expect(Question.daily_questions.last).to eq new_question
+    end
+
+    it 'should not return question older than 1 day' do
+      expect(Question.daily_questions).to_not contain_exactly(old_question)
+    end
+  end
 end
