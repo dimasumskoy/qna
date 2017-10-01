2f15a669faca9ff7e01e8ee6d2346cd245e0404a
dimasumskoy <sumskoydima@gmail.com>
2017-10-01 17:04:07 +0300

    Remake Active Job specs

--- spec/jobs/daily_digest_job_spec.rb
+++ spec/jobs/daily_digest_job_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe DailyDigestJob, type: :job do
-  let(:users) { create_list(:user, 2) }
-  let(:questions) { create_list(:question, 2) }
+  let!(:questions) { create_list(:question, 2) }
 
   it 'should send digest to every user' do
-    users.each do |user|
-      expect(DailyDigestMailer).to receive(:digest).and_call_original
-      DailyDigestMailer.digest(user, questions)
+    User.all.each do |user|
+      expect(DailyDigestMailer).to receive(:digest).with(user, questions).and_call_original
     end
+    DailyDigestJob.perform_now
   end
 end
--- spec/jobs/new_answer_notification_job_spec.rb
+++ spec/jobs/new_answer_notification_job_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 RSpec.describe NewAnswerNotificationJob, type: :job do
   let(:user) { create(:user) }
-  let(:question) { create(:question, user: user) }
+  let(:question) { create(:question) }
+  let!(:subscription) { create(:subscription, question: question, user: user) }
 
   it 'should send notification to user' do
     expect(NotificationMailer).to receive(:new_answer).with(user, question).and_call_original
-    NotificationMailer.new_answer(user, question)
+    NewAnswerNotificationJob.perform_now(question)
   end
 end
