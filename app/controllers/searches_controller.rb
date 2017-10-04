2adb8d696fc70cdbc30607990673664dc63c7f42
dimasumskoy <sumskoydima@gmail.com>
2017-10-04 14:00:16 +0300

    Add partials for results & refactor Search controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,16 @@
 class SearchesController < ApplicationController
+  before_action :set_search_params
   skip_authorization_check
 
   def show
+    @results = ThinkingSphinx.search @search, classes: [@scope]
+    respond_with @results
+  end
+
+  private
+
+  def set_search_params
+    @search = params[:search]
+    @scope  = params[:scope].singularize.constantize unless params[:scope] == 'All'
   end
 end
